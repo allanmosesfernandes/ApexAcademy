be0b3e77e45d6e430e21ccf69bb01a7efede43c2
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-13 18:37:42 +0000

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { StyledHeader } from './header.styles'
+const Header = () => {
+  return (
+    <StyledHeader>
+        <nav>
+            <h2>Apex Academy</h2>
+        </nav>
+    </StyledHeader>
+  )
+}
+
+export default Header
---
+++ src/components/Header/header.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header `
+
+nav {
+display: flex;
+align-items: center;
+justify-content: space-between;
+background-color: red;  
+}
+
+`
