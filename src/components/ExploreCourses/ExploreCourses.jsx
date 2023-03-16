ff5f8a4c1876de4272457df66556344e5a076257
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-16 22:46:01 +0000

    Explore section

--- src/components/ExploreCourses/ExploreCourses.jsx
+++ src/components/ExploreCourses/ExploreCourses.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../styles/container.stylesheet'
+import { ButtonContainer , DisplayCourse, ExploreCoursesSection} from './explorecourses.styles'
+
 const ExploreCourses = () => {
+const [activeButton, setActiveButton] = useState(0);
+
+const handleButtonClick = (index) => {
+  setActiveButton(index);
+};
   return (
-    <Container>
-        <h2>Explore</h2>
-    </Container>
+<Container>
+    <ExploreCoursesSection>
+<h2>Explore our courses</h2>
+    <ButtonContainer>
+      <button onClick={() => handleButtonClick(0)} className={activeButton === 0 ? "active" : ""}>
+
+      </button>
+      <button onClick={() => handleButtonClick(1)} className={activeButton === 1 ? "active" : ""}>2</button>
+      <button onClick={() => handleButtonClick(2)} className={activeButton === 2 ? "active" : ""}>3</button>
+    </ButtonContainer>
+    <DisplayCourse>
+      <div style={{ display: activeButton === 0 ? 'block' : 'none' }}>
+        <h2>this is what shows on button 1</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatum.
+        </p>
+      </div>
+      <div style={{ display: activeButton === 1 ? 'block' : 'none' }}>
+        <h2>this is what shows on button 2</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatum.
+        </p>
+      </div>
+      <div style={{ display: activeButton === 2 ? 'block' : 'none' }}>
+        <h2>this is what shows on button 3</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatum.
+        </p>
+      </div>
+    </DisplayCourse>
+    </ExploreCoursesSection>
+    
+  </Container>
   )
 }
 
--- src/components/ExploreCourses/explorecourses.styles.js
+++ src/components/ExploreCourses/explorecourses.styles.js
@@ -1,1 +1,26 @@
 import styled from "styled-components";
+
+
+export const ExploreCoursesSection = styled.section`
+h2 {
+    font-size: ${(props) => props.theme.fsxl};
+    font-family: "Neue Bold";
+    color: #fff;
+}
+@media only screen and (max-width: 600px) {
+  h2 {
+    font-size: 4rem;
+  }
+}
+`
+export const ButtonContainer = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr;
+gap: 1rem;
+
+`
+export const DisplayCourse = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr;
+gap: 1rem;
+`
