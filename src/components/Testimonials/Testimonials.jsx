0059883e4e2269f43523e71322fd1380f84050e9
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-17 16:06:45 +0000

    Separate-Slider-Component

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import Container from '../styles/container.stylesheet'
 import { TestimonialDiv } from './testimonials.styles'
-// Import css files
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import SliderReviews from '../SliderReviews/SliderReviews'
+
 
 const Testimonials = () => {
   return (
     <Container>
-        <h2>Testimonials </h2>
-        <TestimonialDiv>
-
+      <h2>Testimonials </h2>
+      <TestimonialDiv>
         <p>See what our students say</p>
-        </TestimonialDiv>
-
+        <SliderReviews />
+      </TestimonialDiv>
     </Container>
   )
 }
