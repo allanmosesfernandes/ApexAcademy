67b9a81d6046830284e44c34cf1ef6f6118ecec5
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-17 13:54:39 +0000

    JSON-map

--- src/components/ExploreCourses/ExploreCourses.jsx
+++ src/components/ExploreCourses/ExploreCourses.jsx
@@ -1,22 +1,19 @@
-import React, {
-    useState
-} from 'react'
+import React, { useState } from 'react'
 import Container from '../styles/container.stylesheet'
-import {
-    ButtonContainer,
-    DisplayCourse,
-    ExploreCoursesSection,
-    CourseBtn,
-    CourseCardsContainer,
-    CourseCard
-} from './explorecourses.styles'
+import { ButtonContainer , DisplayCourse, ExploreCoursesSection,CourseBtn,CourseCardsContainer,CourseCard} from './explorecourses.styles'
 import Stats from "../../assets/images/EXPLORE/calc.svg";
-import Stats from "../../assets/images/EXPLORE/calc.svg";
+import Dollar from "../../assets/images/EXPLORE/worldDOllar.svg";
+import Calculator from "../../assets/images/EXPLORE/calculator.svg";
+import Coins from "../../assets/images/EXPLORE/coins.svg";
+import Ruppee from "../../assets/images/EXPLORE/ruppe-bag.svg";
+import Chart from "../../assets/images/EXPLORE/graph.svg";
+import Cart from "../../assets/images/EXPLORE/cart.svg";
+
 
 const ExploreCourses = () => {
-    const [activeButton, setActiveButton] = useState(0);
-
-    const [courseDetails, setCourseDetails] = useState([{
+const [activeButton, setActiveButton] = useState(0);
+    const [courseDetails, setCourseDetails] = useState([
+        {
             courseTitle: "FYJC",
             subjects: [
                 {
@@ -25,151 +22,137 @@ const ExploreCourses = () => {
                 },
                 {
                     name: "S.P",
-                    image: ScienceImage
+                    image: Dollar
                 },
                 {
                     name: "Organisation of Commerce",
-                    image: MathsImage
+                    image: Chart
                 },
                 {
                     name: "Economics",
-                    image: ScienceImage
+                    image: Coins
                 },
                 {
                     name: "Book-Keeping and Accountancy",
-                    image: ScienceImage
+                    image: Ruppee
                 },
             ]
         },
         {
             courseTitle: "SYJC",
-            subjects: [{
+            subjects: [
+                {
+                    name: "Mathematics and Statistics",
+                    image: Stats
+                },
+                {
+                    name: "S.P",
+                    image: Dollar
+                },
+                {
                     name: "Organisation of Commerce",
-                    image: MathsImage
+                    image: Chart
                 },
                 {
                     name: "Economics",
-                    image: ScienceImage
-                }
+                    image: Coins
+                },
+                {
+                    name: "Book-Keeping and Accountancy",
+                    image: Ruppee
+                },
             ]
         },
         {
-            courseTitle: "FYJC",
-            subjects: [{
+            courseTitle: "FYBCOM",
+            subjects: [
+                {
                     name: "Mathematics and Statistics",
-                    image: MathsImage
+                    image: Stats
                 },
                 {
-                    name: "Science",
-                    image: ScienceImage
-                }
-            ]
-        },
-        {
-            courseTitle: "FYJC",
-            subjects: [{
-                    name: "Mathematics and Statistics",
-                    image: MathsImage
+                    name: "S.P",
+                    image: Dollar
                 },
                 {
-                    name: "Science",
-                    image: ScienceImage
-                }
+                    name: "Organisation of Commerce",
+                    image: Chart
+                },
+                {
+                    name: "Economics",
+                    image: Coins
+                },
+                {
+                    name: "Book-Keeping and Accountancy",
+                    image: Ruppee
+                },
             ]
         },
 
     ]);
 
+   
+const handleButtonClick = (index) => {
+  setActiveButton(index);
+};
+  return (
+<Container>
+<ExploreCoursesSection>
+<h2>Explore our courses</h2>
+    <ButtonContainer>
+      <CourseBtn onClick={() => handleButtonClick(0)} className={activeButton === 0 ? "active" : ""}> FYJC </CourseBtn>
+      <CourseBtn onClick={() => handleButtonClick(1)} className={activeButton === 1 ? "active" : ""}>SYJC</CourseBtn>
+      <CourseBtn onClick={() => handleButtonClick(2)} className={activeButton === 2 ? "active" : ""}>BAF</CourseBtn>
+    </ButtonContainer>
+    <DisplayCourse>
 
-    const handleButtonClick = (index) => {
-        setActiveButton(index);
-    };
-    return ( <
-        Container >
-        <
-        ExploreCoursesSection >
-        <
-        h2 > Explore our courses < /h2> <
-        ButtonContainer >
-        <
-        CourseBtn onClick = {
-            () => handleButtonClick(0)
-        }
-        className = {
-            activeButton === 0 ? "active" : ""
-        } > FYJC < /CourseBtn> <
-        CourseBtn onClick = {
-            () => handleButtonClick(1)
-        }
-        className = {
-            activeButton === 1 ? "active" : ""
-        } > SYJC < /CourseBtn> <
-        CourseBtn onClick = {
-            () => handleButtonClick(2)
-        }
-        className = {
-            activeButton === 2 ? "active" : ""
-        } > BAF < /CourseBtn> <
-        /ButtonContainer> <
-        DisplayCourse >
+      <CourseCardsContainer>
+        <CourseCard className={activeButton === 0 ? 'active' : '' }>
+<div className="course-card-flex-container">
+  {courseDetails
+    .filter((course) => course.courseTitle === "FYJC")
+    .map((course) =>
+      course.subjects.map((subject) => (
+        <div className="course-card">
+          <img src={subject.image} alt={subject.name} />
+          <p>{subject.name}</p>
+        </div>
+      ))
+    )}
+</div>
 
-        <
-        CourseCardsContainer >
-        <
-        CourseCard className = {
-            activeButton === 0 ? 'active' : ''
-        } >
-        <
-        div className = "course-card-flex-container" >
-        <
-        div className = "course-card-pill" >
-        <
-        img src = ""
-        alt = "" / >
-        <
-        h4 > Book - Keeping and Accountancy < /h4> <
-        /div> <
-        /div> <
-        /CourseCard> <
-        CourseCard className = {
-            activeButton === 1 ? 'active' : ''
-        } >
-        <
-        div className = "course-card-flex-container" >
-        <
-        div className = "course-card-pill" >
-        <
-        img src = ""
-        alt = "" / >
-        <
-        h4 > Book - Keeping and Accountancy < /h4> <
-        /div> <
-        /div> <
-        /CourseCard> <
-        CourseCard className = {
-            activeButton === 2 ? 'active' : ''
-        } >
-        <
-        div className = "course-card-flex-container" >
-        <
-        div className = "course-card-pill" >
-        <
-        img src = ""
-        alt = "" / >
-        <
-        h4 > Book - Keeping and Accountancy < /h4> <
-        /div> <
-        /div> <
-        /CourseCard> <
-        /CourseCardsContainer>
 
-        <
-        /DisplayCourse> <
-        /ExploreCoursesSection>
+        </CourseCard>
+        <CourseCard className={activeButton === 1 ? 'active' : '' }>
+<div className="course-card-flex-container">
+  {courseDetails
+    .filter((course) => course.courseTitle === "FYJC")
+    .map((course) =>
+      course.subjects.map((subject) => (
+        <div className="course-card">
+          <img src={subject.image} alt={subject.name} />
+          <h3>{subject.name}</h3>
+        </div>
+      ))
+    )}
+</div>
 
-        <
-        /Container>
-    )
+        </CourseCard>
+        <CourseCard className={activeButton === 2 ? 'active' : '' }>
+            <div className="course-card-flex-container">
+                <div className="course-card-pill">
+                    <img src="" alt="" />
+                    <h4>Book-Keeping and Accountancy</h4>
+                </div>
+            </div>
+        </CourseCard>
+      </CourseCardsContainer>
+
+    </DisplayCourse>
+    </ExploreCoursesSection>
+    
+  </Container>
+  )
 }
 
 export default ExploreCourses
