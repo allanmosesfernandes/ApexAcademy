541bdc2c519e4b0e439cc72f6f419469a4cf59c1
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-21 14:13:50 +0000

    Contact-SVG

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Container from '../styles/container.stylesheet'
-import { ContactSection } from './contact.styles'
+import { ContactSection, Contact__Left, Contact__Right } from './contact.styles'
 const Contact = () => {
   return (
     <Container>
         <ContactSection>
-        <h2>Get in touch</h2>
-        <p>
-            asdfasdf
-        </p>
+            <Contact__Left>
+                <h2>Get in touch</h2>
+            </Contact__Left>
+            <Contact__Right>
+                <div className="contact__block">
+                    <img></img>
+                </div>
+            </Contact__Right>
         </ContactSection>
 
     </Container>
--- src/components/Contact/contact.styles.js
+++ src/components/Contact/contact.styles.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const ContactSection = styled.section`
+display: grid;
+grid-template-column: 1fr 2fr;
+gap: 2rem;
 margin: 5rem 0 3rem 0;
 h2 {
     color:  ${(props) => props.theme.primaryColor};
@@ -9,5 +12,15 @@ p {
     font-size: ${(props) => props.theme.fssmall};
     color: #000;
     margin: 2rem 0;
+}
+`
+
+export const Contact__Left  = styled.div`
+
+`
+
+export const Contact__Right  = styled.div`
+.contact__block {
+
 }
 `
