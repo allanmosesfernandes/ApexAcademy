e855a7afe8ecafb847fbd74e0d201427b41e307c
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-21 16:52:42 +0000

    Contact-touchups

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import Container from '../styles/container.stylesheet'
 import { ContactSection, Contact__Left, Contact__Right } from './contact.styles';
-import AddressSVG from "../../assets/images/Contact/location-pin.svg";
+import AddressSVG from "../../assets/images/Contact/location_pin.svg";
 import CallSVG from "../../assets/images/Contact/call.svg";
 import ThumbSVG from "../../assets/images/Contact/thumbs.svg";
-
+import Whatsapp from "../../assets/images/Contact/whatsapp.svg";
+import Instagram from "../../assets/images/Contact/instagram.svg";
+import { Link } from 'react-router-dom';
+import PhoneCircle from "../../assets/images/Contact/phone-circle.svg"
 const Contact = () => {
   return (
     <Container>
         <ContactSection>
             <Contact__Left>
                 <h2>Get in touch</h2>
+                <div className="pattern-bg">
+                    
+                </div>
             </Contact__Left>
             <Contact__Right>
                 <div className="contact__block">
-                    <img src={AddressSVG} alt="Address Pin"/>
+                    <Link to="https://goo.gl/maps/kSWe1kddnV7F5es27">
+                        <img src={AddressSVG} alt="Address Pin"/>
+                    </Link>
                     <div className="contact__contents">
-                        <h3>Address</h3>
+                        <Link to="https://goo.gl/maps/kSWe1kddnV7F5es27">
+                            <h3>Address</h3>
+                        </Link>
+                        
                         <p>
                             Mumbai<br />
                             Room #5, Coelho Chawl Juhu <br />
@@ -25,14 +36,20 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="contact__block">
-                    <img src={CallSVG} alt="Address Pin"/>
+                    <Link to="tel:+918591829604">
+                        <img src={PhoneCircle} alt="Contact Pin"/>
+                    </Link>
+
                     <div className="contact__contents">
                         <h3>Phone</h3>
-                        <p>+91 9384026784</p>
+                        <p>+91 8591829604</p>
                     </div>
                 </div>
                 <div className="contact__block">
-                    <img src={AddressSVG} alt="Address Pin"/>
+                    <Link to="https://wa.me/919619303386">
+                        <img src={Whatsapp} alt="Whatsapp Logo"/>
+                    </Link>
+
                     <div className="contact__contents">
                         <h3>Query</h3>
                         <p>
@@ -41,9 +58,13 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="contact__block">
-                    <img src={ThumbSVG} alt="Address Pin"/>
+                    <Link to="https://www.instagram.com/its_ganeshm_12/">
+                        <img src={Instagram} alt="Instagram Logo"/>
+                    </Link>
                     <div className="contact__contents">
-                        <h3>Socials</h3>
+                        <h3>
+                            <Link to="https://www.instagram.com/its_ganeshm_12/">Socials</Link>
+                        </h3>
                         <p>
                            Follow us on Instagram!
                         </p>
--- src/components/Contact/contact.styles.js
+++ src/components/Contact/contact.styles.js
@@ -13,19 +13,41 @@ p {
     color: #000;
     margin: 2rem 0;
 }
+
+@media only screen and (max-width: 600px) {
+  grid-template-columns: 1fr;
+
+  h2 {
+    font-size: 
+  }
+}
 `
 
 export const Contact__Left  = styled.div`
+.pattern-bg {
+width: 100%;
+height: 50%;
+    background-color: #ffffff;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='12' viewBox='0 0 20 12'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='charlie-brown' fill='%234353ff' fill-opacity='0.4'%3E%3Cpath d='M9.8 12L0 2.2V.8l10 10 10-10v1.4L10.2 12h-.4zm-4 0L0 6.2V4.8L7.2 12H5.8zm8.4 0L20 6.2V4.8L12.8 12h1.4zM9.8 0l.2.2.2-.2h-.4zm-4 0L10 4.2 14.2 0h-1.4L10 2.8 7.2 0H5.8z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 
+&:hover{
+    transform: rotate(2deg)
+}
+}
 `
 
 export const Contact__Right  = styled.div`
 .contact__block {
 display: flex;
-gap: 2rem;
+gap: 3rem;
 padding: 2rem 0;
 align-items: flex-start;
 border-bottom: 1px solid gray;
+
+&:last-child {
+    border-bottom: 0;
+}
+
 .contact__contents {
 
     h3 {
@@ -37,6 +59,19 @@ border-bottom: 1px solid gray;
     }
     display: flex;
     flex-direction: column;
+}
+@media only screen and (max-width: 600px) {
+  //code goes here
+  .contact__contents {
+    h3 {
+        font-size: ${(props) => props.theme.fssmall};
+    }
+    p {
+        font-size: ${(props) => props.theme.fsxsmall};
+    }
+  }
+
+
 }
 }
 `
