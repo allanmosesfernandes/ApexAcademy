8bfa753c5afdcdc857fa0127f134d54430768c71
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-21 15:03:33 +0000

    Contact-Details

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Container from '../styles/container.stylesheet'
-import { ContactSection, Contact__Left, Contact__Right } from './contact.styles'
+import { ContactSection, Contact__Left, Contact__Right } from './contact.styles';
+import AddressSVG from "../../assets/images/Contact/location-pin.svg"
 const Contact = () => {
   return (
     <Container>
@@ -10,7 +11,34 @@ const Contact = () => {
             </Contact__Left>
             <Contact__Right>
                 <div className="contact__block">
-                    <img></img>
+                    <img src={AddressSVG} alt="Address Pin"/>
+                    <div className="contact__contents">
+                        <h3>Address</h3>
+                        <p>
+                            Mumbai<br />
+                            Room #5, Coelho Chawl Juhu
+                        </p>
+                    </div>
+                </div>
+                <div className="contact__block">
+                    <img src={AddressSVG} alt="Address Pin"/>
+                    <div className="contact__contents">
+                        <h3>Address</h3>
+                        <p>
+                            Mumbai<br />
+                            Room #5, Coelho Chawl Juhu
+                        </p>
+                    </div>
+                </div>
+                <div className="contact__block">
+                    <img src={AddressSVG} alt="Address Pin"/>
+                    <div className="contact__contents">
+                        <h3>Address</h3>
+                        <p>
+                            Mumbai<br />
+                            Room #5, Coelho Chawl Juhu
+                        </p>
+                    </div>
                 </div>
             </Contact__Right>
         </ContactSection>
--- src/components/Contact/contact.styles.js
+++ src/components/Contact/contact.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ContactSection = styled.section`
 display: grid;
-grid-template-column: 1fr 2fr;
+grid-template-columns: 1.5fr 2fr;
 gap: 2rem;
 margin: 5rem 0 3rem 0;
 h2 {
@@ -21,6 +21,22 @@ export const Contact__Left  = styled.div`
 
 export const Contact__Right  = styled.div`
 .contact__block {
+display: flex;
+gap: 2rem;
+padding: 2rem 0;
+align-items: flex-start;
+border-bottom: 1px solid gray;
+.contact__contents {
 
+    h3 {
+        font-size: ${(props) => props.theme.fsmedium};
+    }
+
+    p {
+        margin: 1rem 0;
+    }
+    display: flex;
+    flex-direction: column;
+}
 }
 `
