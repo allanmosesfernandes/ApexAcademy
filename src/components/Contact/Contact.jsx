80a61595338bd1fb55effb30f8e372d0d12dedf9
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-21 15:12:29 +0000

    Contact-Form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Container from '../styles/container.stylesheet'
 import { ContactSection, Contact__Left, Contact__Right } from './contact.styles';
-import AddressSVG from "../../assets/images/Contact/location-pin.svg"
+import AddressSVG from "../../assets/images/Contact/location-pin.svg";
+import CallSVG from "../../assets/images/Contact/call.svg";
+import ThumbSVG from "../../assets/images/Contact/thumbs.svg";
+
 const Contact = () => {
   return (
     <Container>
@@ -16,27 +19,33 @@ const Contact = () => {
                         <h3>Address</h3>
                         <p>
                             Mumbai<br />
-                            Room #5, Coelho Chawl Juhu
+                            Room #5, Coelho Chawl Juhu <br />
+                            Santacruz West 400049
                         </p>
+                    </div>
+                </div>
+                <div className="contact__block">
+                    <img src={CallSVG} alt="Address Pin"/>
+                    <div className="contact__contents">
+                        <h3>Phone</h3>
+                        <p>+91 9384026784</p>
                     </div>
                 </div>
                 <div className="contact__block">
                     <img src={AddressSVG} alt="Address Pin"/>
                     <div className="contact__contents">
-                        <h3>Address</h3>
+                        <h3>Query</h3>
                         <p>
-                            Mumbai<br />
-                            Room #5, Coelho Chawl Juhu
+                           Send us a message on Whatsapp <br/> and our team will get in touch with you!
                         </p>
                     </div>
                 </div>
                 <div className="contact__block">
-                    <img src={AddressSVG} alt="Address Pin"/>
+                    <img src={ThumbSVG} alt="Address Pin"/>
                     <div className="contact__contents">
-                        <h3>Address</h3>
+                        <h3>Socials</h3>
                         <p>
-                            Mumbai<br />
-                            Room #5, Coelho Chawl Juhu
+                           Follow us on Instagram!
                         </p>
                     </div>
                 </div>
