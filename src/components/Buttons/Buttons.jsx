a4e16540ec1685cae9e167eadcd0e9bdff1aedcc
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-22 15:12:10 +0000

    button CTA

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import StyledButton from './buttons.styles'
+import React from 'react';
+import StyledButton from './buttons.styles';
+import Whatsapp from "../../assets/images/Contact/whatsapp.svg"
+
+
 const Buttons = (props) => {
 const { text, type } = props;
   return (
     <StyledButton className='offset'>
+      <img src={Whatsapp} alt="whatsapp" /> 
       Whatsapp Now
     </StyledButton>
   )
--- src/components/Buttons/buttons.styles.js
+++ src/components/Buttons/buttons.styles.js
@@ -4,21 +4,31 @@ import NeueExtraBold from "../../assets/fonts/NM-Ultra.otf";
 
 const StyledButton = styled.button`
 background-color: transparent;
+display: flex;
+align-items: center;
+gap: 2rem;
 font-family: Neue Bold;
 padding: 2rem 3rem;
-color: black;
+margin: 2rem 0;
+color: ${(props) => props.theme.themeGreen};
+font-weight: bold;
 font-size: 1.5rem;
-  box-shadow: 
-    0.3em 0.3em 0 0 green,
-    inset 0.3em 0.3em 0 0 green;
-  transition: all 0.3s ease;
+box-shadow: 
+0.3em 0.3em 0 0 green,
+inset 0.3em 0.3em 0 0 green;
+transition: all 0.5s ease;
+
 &:hover,
 &:focus {
   box-shadow: 
     0 0 0 0 white,
     inset 0 0 0 100px green;
+    color: white;
 }
 
+img {
+    width: 20px;
+}
 `
 
 export default StyledButton
