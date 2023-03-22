7f4ab33ebc71dd92105e2cf1a82dd2fbe5f4a0ae
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-22 17:09:46 +0000

    About-page-responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Container from '../styles/container.stylesheet'
-import { AboutWrapper, AboutImageText,AboutStoryContainer } from './about-styles'
+import { AboutWrapper, AboutImageText,AboutStoryContainer,Story,TextPiece,SentiImage } from './about-styles'
 
 import Pexel from "../../assets/images/pexel.jpg";
+import Senti from "../../assets/images/senti.jpg";
 const About = () => {
   return (
     <Container>
@@ -20,6 +21,18 @@ const About = () => {
               </p>
             </AboutImageText>
             <AboutStoryContainer>
+                <Story className='a'>
+                    <h4>Story</h4>
+                </Story>
+                <TextPiece className='b'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam vero architecto ipsa provident porro hic facere officiis, dignissimos eveniet labore.<br/>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt alias neque quisquam recusandae eaque tenetur veniam tempore quis laboriosam corporis! <br />
+                        Lorem ipsum dolor sit amet.
+                    </p>
+
+                </TextPiece>
+                <SentiImage className='c' src={Senti} alt="Senti" />
 
             </AboutStoryContainer>
         </AboutWrapper>
--- src/components/About/about-styles.js
+++ src/components/About/about-styles.js
@@ -19,18 +19,86 @@ export const AboutImageText = styled.div`
 
 display: grid;
 grid-template-columns: 1fr 1fr;
-
+border-bottom: 1px solid black;
 img {
     width: 90%;
+    border-right: 1px solid black;
+    filter: blur(2px);
 }
 p {
     font-size: ${props => props.theme.fssmall};
     align-items: center;
     display: flex;
+    border-bottom: 0
+}
+
+@media only screen and (max-width: 600px) {
+  //code goes here
+  grid-template-columns: 1fr;
+
+  img {
+    width: 100%;
+}
+p {
+    text-align: center;
+}
 }
 `
 
 export const AboutStoryContainer = styled.div`
+display: grid;
+grid-template-columns: 100px 2fr 1fr;
+gap: 1rem;
+
+@media only screen and (max-width: 600px) {
+  //code goes here
+  grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto auto;
+
+  .a {
+    grid-row: 2;
+    grid-column: 1;
+  }
+
+  .b {
+    grid-row: 2;
+    grid-column: 2;
+  }
+
+  .c {
+    grid-row: 1;
+    grid-column: 1 / span 2;
+  }
+}
+`
+
+export const Story = styled.div`
+border-right: 1px solid black;
+h4 {
+    writing-mode: tb-rl;
+    transform: rotate(-180deg);
+    border: none;
+    padding: 2rem;
+font-size: ${props => props.theme.fslarge};
+
+@media only screen and (max-width: 600px) {
+  font-size: ${props => props.theme.fssmall};
+    padding: 1rem;
+
+}
+}
+
+`
+export const TextPiece = styled.div`
+p {
+    font-size: ${props => props.theme.fsxsmall};
+    height: 100%;
+    display: flex;
+    align-items:center;
+}
+`
+
+export const SentiImage = styled.img`
 
 
 `
