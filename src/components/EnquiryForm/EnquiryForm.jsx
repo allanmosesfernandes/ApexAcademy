b8e50852c288e96f416d98b1d64250aa8d4d346f
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-28 02:56:28 +0100

    testing-dummy-form

--- src/components/EnquiryForm/EnquiryForm.jsx
+++ src/components/EnquiryForm/EnquiryForm.jsx
@@ -58,7 +58,28 @@ const handleSubmit = async (e) => {
             <p>
                 Fill out the form below and we'll get in touch shortly
             </p>
-            <EnquiresFormStyled  
+            <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
+            {/* <EnquiresFormStyled  
                 data-netlify="true" 
                 name="enquries"   
                 method="POST"
@@ -121,7 +142,7 @@ const handleSubmit = async (e) => {
                     />
                 </PhoneInputWrapper>
             <button type="submit">Submit</button>
-            </EnquiresFormStyled>
+            </EnquiresFormStyled> */}
             </div>
 
 
