56e17a72dc776cf1ec07ba0adbbb41f144d22211
Allan <fernzallan14@gmail.com>
2023-05-23 12:40:04 +0100

    Netlify form issue

--- src/components/EnquiryForm/EnquiryForm.jsx
+++ src/components/EnquiryForm/EnquiryForm.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Container from '../styles/container.stylesheet'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { FormDivWrapper, EnquiresFormStyled, PhoneInputWrapper } from './enquiryForm.styles'
 import { useNavigate } from 'react-router-dom';
 import GoogleMap from '../GoogleMaps/GoogleMap';
 
 const EnquiryForm = () => {
     const navigate = useNavigate();
+    const formRef = useRef(null);
 
     const defaultFormFields = {
         firstName: "",
@@ -25,56 +26,79 @@ const EnquiryForm = () => {
         setFormFields({...formFields, [name]:value})
     }
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => navigate("/"))
-    .catch((error) => alert(error));
-};
-
-
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(formFields);
+    
+      const encodedFormData = Object.keys(formFields)
+        .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(formFields[key]))
+        .join('&');
+    
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encodedFormData,
+      })
+        .then(() => navigate('/'))
+        .catch((error) => alert(error));
+    };
+    
   return (
     <Container>
       <FormDivWrapper>
-      <form name="contact" method="post" data-netlify='true'>
-      <input type="hidden" name="form-name" value="contact" />
+      <h2>Enquiries</h2>
+      <p>Fill out the form below and we'll get in touch shortly</p>
+      <EnquiresFormStyled
+        name='contact'
+        method='POST'
+        data-netlify='true' ref={formRef}>
 
-          <p>
-            <label>
-              Your Name: <input type='text' name='name' />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type='email' name='email' />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Role:{' '}
-              <select name='role[]' multiple>
-                <option value='leader'>Leader</option>
-                <option value='follower'>Follower</option>
-              </select>
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name='message'></textarea>
-            </label>
-          </p>
-          <p>
-            <button type='submit'>Send</button>
-          </p>
-        </form>
+        <input type="hidden" name="contact" value="contact" />
+        <input
+          type='text'
+          name='firstName'
+          placeholder='First Name'
+          pattern='^[a-zA-Z\s-]+$'
+          title='Name cannot contain special characters'
+          required
+          onChange={handleFormValueChange}
+        />
+        <input
+          type='text'
+          name='lastName'
+          placeholder='Last Name'
+          pattern='^[a-zA-Z\s-]+$'
+          title='Name cannot contain special characters'
+          required
+          onChange={handleFormValueChange}
+        />
+            <input type='email' name='email' placeholder='Email address' onChange={handleFormValueChange}/>
+
+            <select name='courseSelection' defaultValue='' onChange={handleFormValueChange}>
+              <option value='' disabled>
+                Select your course
+              </option>
+              <option value='FYJC'>FYJC</option>
+              <option value='SYJC'>SYJC</option>
+              <option value='FYBCOM'>FYBCOM</option>
+              <option value='SYBCOM'>SYBCOM</option>
+              <option value='TYBCOM'>TYBCOM</option>
+              <option value='BMS'>BMS</option>
+            </select>
+
+            <PhoneInputWrapper>
+              <input
+                type='number'
+                placeholder='Mobile Number'
+                name='contactNumber'
+                required
+                pattern='^[789]\d{9}$'
+                title='Enter a valid mobile number'
+                onChange={handleFormValueChange}
+              />
+            </PhoneInputWrapper>
+            <button type='submit' onClick={handleSubmit}>Submit</button>
+          </EnquiresFormStyled>
 
         {/* <div id='form'>
           <h2>Enquiries</h2>
