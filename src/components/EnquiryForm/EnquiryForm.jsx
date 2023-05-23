7daecad39a124e6c6d07a95021282219718ba7b9
Allan <fernzallan14@gmail.com>
2023-05-23 14:23:36 +0100

    Form-name encoded

--- src/components/EnquiryForm/EnquiryForm.jsx
+++ src/components/EnquiryForm/EnquiryForm.jsx
@@ -26,22 +26,22 @@ const EnquiryForm = () => {
         setFormFields({...formFields, [name]:value})
     }
 
-//     const handleSubmit = (event) => {
-//       event.preventDefault();
-//       console.log(formFields);
-//     
-//       const encodedFormData = Object.keys(formFields)
-//         .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(formFields[key]))
-//         .join('&');
-//       fetch('/', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-//         body: encodedFormData,
-//       })
-//       .then(() => alert("Success!"))
-//       .catch(error => alert(error));
-// 
-//     };
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(formFields);
+    
+      const encodedFormData = Object.keys(formFields)
+        .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(formFields[key]))
+        .join('&');
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({ "form-name": "contact", encodedFormData })        
+      })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    };
     
   return (
     <Container>
@@ -52,7 +52,7 @@ const EnquiryForm = () => {
         name='contact'
         method='POST'
         data-netlify='true'
-        onSubmit={handleSubmit}>
+        onSubmit={handleSubmit} >
         <input
           type='text'
           name='firstName'
@@ -60,6 +60,7 @@ const EnquiryForm = () => {
           pattern='^[a-zA-Z\s-]+$'
           title='Name cannot contain special characters'
           required
+          onChange={handleFormValueChange}
         />
         <input
           type='text'
@@ -68,90 +69,11 @@ const EnquiryForm = () => {
           pattern='^[a-zA-Z\s-]+$'
           title='Name cannot contain special characters'
           required
+          onChange={handleFormValueChange}
         />
-        <input type='email' name='email' placeholder='Email address'/>
-
-            {/* <select name='courseSelection' defaultValue='' onChange={handleFormValueChange}>
-              <option value='' disabled>
-                Select your course
-              </option>
-              <option value='FYJC'>FYJC</option>
-              <option value='SYJC'>SYJC</option>
-              <option value='FYBCOM'>FYBCOM</option>
-              <option value='SYBCOM'>SYBCOM</option>
-              <option value='TYBCOM'>TYBCOM</option>
-              <option value='BMS'>BMS</option>
-            </select>
-
-            <PhoneInputWrapper>
-              <input
-                type='number'
-                placeholder='Mobile Number'
-                name='contactNumber'
-                required
-                pattern='^[789]\d{9}$'
-                title='Enter a valid mobile number'
-                onChange={handleFormValueChange}
-              />
-            </PhoneInputWrapper>
-            <button type='submit'>Submit</button>
-          </EnquiresFormStyled>
-
-        {/* <div id='form'>
-          <h2>Enquiries</h2>
-          <p>Fill out the form below and we'll get in touch shortly</p>
-
-          <EnquiresFormStyled
-            name='enquries'
-            method='POST'
-            data-netlify='true'
-            action='/about'
-            id='enquries'
-          >
-            <input type='hidden' name='enquries' value='enquries' />
-            <input
-              type='text'
-              name='firstName'
-              placeholder='First Name'
-              pattern='^[a-zA-Z\s-]+$'
-              title='Name cannot contain special characters'
-              required
-            />
-            <input
-              type='text'
-              name='lastName'
-              placeholder='Last Name'
-              pattern='^[a-zA-Z\s-]+$'
-              title='Name cannot contain special characters'
-              required
-            />
-            <input type='email' name='email' placeholder='Email address' />
-
-            <select name='courseSelection' defaultValue=''>
-              <option value='' disabled>
-                Select your course
-              </option>
-              <option value='FYJC'>FYJC</option>
-              <option value='SYJC'>SYJC</option>
-              <option value='FYBCOM'>FYBCOM</option>
-              <option value='SYBCOM'>SYBCOM</option>
-              <option value='TYBCOM'>TYBCOM</option>
-              <option value='BMS'>BMS</option>
-            </select>
-
-            <PhoneInputWrapper>
-              <input
-                type='number'
-                placeholder='Mobile Number'
-                name='contactNumber'
-                required
-                pattern='^[789]\d{9}$'
-                title='Enter a valid mobile number'
-              />
-            </PhoneInputWrapper>
-            <button type='submit'>Submit</button>
-          </EnquiresFormStyled>
-        </div> */}
+        <input type='email' name='email' placeholder='Email address' onChange={handleFormValueChange} />
+        <button type='submit'>Submit</button>
+        </EnquiresFormStyled>
         <GoogleMap />
       </FormDivWrapper>
     </Container>
