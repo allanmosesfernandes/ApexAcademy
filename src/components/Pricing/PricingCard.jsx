aa6027c7045bce79fe03b2be7ceeee6120874df7
Allan Fernandes <Fernzallan14@gmail.com>
2023-05-29 11:39:37 +0100

    Pricing-card-logic

--- src/components/Pricing/PricingCard.jsx
+++ src/components/Pricing/PricingCard.jsx
@@ -9,26 +9,31 @@ const PricingCard = (props) => {
     navigate('/contact');
     window.scrollTo(0, 0);
   };
-    const { title, price, active, onClick, marketPrice, monthly } = props
+    const {
+      title,
+      price,
+      active,
+      onClick,
+      marketPrice,
+      monthly,
+      showMonthlyPrice,
+    } = props
     
     const handleClick = () =>{
         onClick(title);
     }
+    const displayPrice = showMonthlyPrice ? monthly : price;
 
     return (
       <>
-
-
         <PricingCardStyles
           active={active}
           onClick={handleClick}
-          marketPrice={marketPrice}>
+          marketPrice={marketPrice}
+        >
           <h3>{title}</h3>
-          <h2>₹{price}</h2>
-          <h4>OR</h4>
-          <h3>
-            ₹{monthly} <span>/per month</span>
-          </h3>
+          <h2>₹{displayPrice}</h2>
+
           <p>What's included</p>
           <ul>
             <li>Self paced learning modules</li>
